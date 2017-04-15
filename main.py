316367ee7c18ae1b510fb36b6632c96f6d1eab44
John Garrett <john.garrett718@gmail.com>
2017-04-15 14:57:27 -0500

    third commit

--- main.py
+++ main.py
@@ -1,25 +1,49 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+import os
 import webapp2
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render (self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class Content(db.Model):
+    title = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class MainPage(Handler):
+    def render_newpost(self, title="", content="", error=""):
+        blogs = db.GqlQuery("SELECT * FROM Content ORDER BY created DESC")
+        self.render("newpost.html", title=title, content=content, error=error, blogs=blogs)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.render_newpost()
+
+    def post(self):
+        title = self.request.get("title")
+        content = self.request.get("content")
+
+        if title and content:
+            a = Content(title = title, content = content)
+            a.put()
+
+            self.redirect("/")
+        else:
+            error = "We need a subject and some content!"
+            self.render_newpost(title, content, error)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+    ('/', MainPage),
+    ], debug=True)
